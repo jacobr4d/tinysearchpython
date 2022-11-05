18cdd7813aa80c95bcfa8c9857b754ca9acc283d
Jake Glenn <jbradleyglenn@gmail.com>
2022-11-05 15:31:41 -0400

    better argparse

--- tinysearchpython/crawler/crawler.py
+++ tinysearchpython/crawler/crawler.py
@@ -2,6 +2,7 @@ import sys
 import re
 import atexit
 import argparse
+import logging
 
 import requests
 from lxml.html import iterlinks, make_links_absolute, fromstring
@@ -15,39 +16,41 @@ from robot import *
 ps = nltk.stem.PorterStemmer()
 stop_words_set = set(stopwords.words("english"))
 
-# parse arguments used to config crawling
+# crawl config is args
 parser = argparse.ArgumentParser(prog="tinysearchpython", description="crawl, index, pagerank, search engine")
-parser.add_argument("--hits", default="hits", help="location to store hits")
-parser.add_argument("--links", default="links", help="location to store links")
-parser.add_argument("--bpp", default="1000000", help="page size limit bytes (not processed if over)")
-parser.add_argument("--hpp", default="1000000", help="hits per page limit (subset processed if over)")
-parser.add_argument("--lpp", default="1000000", help="links per page limit (subset processed if over)")
+parser.add_argument("--seeds", dest="seeds_path", default="seeds", help="location to get seed urls")
+parser.add_argument("--hits", dest="hits_path", default="hits", help="location to store hits")
+parser.add_argument("--links", dest="links_path", default="links", help="location to store links")
+parser.add_argument("--bpp", dest="page_size_limit_bytes", type=int, default=1000000, help="page size limit bytes (not processed if over)")
+parser.add_argument("--hpp", dest="hits_per_page_limit", type=int, default=1000000, help="hits per page limit (subset processed if over)")
+parser.add_argument("--lpp", dest="links_per_page_limit", type=int, default=1000000, help="links per page limit (subset processed if over)")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 args = parser.parse_args(sys.argv[1:])
+if args.verbose:
+    logging.basicConfig(level=logging.INFO)
 
-hits_path = args.hits
-links_path = args.links
-page_size_limit_bytes = int(args.bpp)
-hits_per_page_limit = int(args.hpp)
-links_per_page_limit = int(args.lpp)
-
-# init crawl
-count = 0
+# crawl state
+frontier = [Url(x.strip()) for x in open(args.seeds_path).readlines()]
+seen = set(str(x) for x in frontier)
 robots = {}
-frontier = [Url("https://en.wikipedia.org/wiki/Homo_antecessor")]
-seen = set(str(x) for x in frontier)                                  # set of url strings
+count = 0
+
+# dump log at end
 def log():
-    print(f"hits path {hits_path}")
-    print(f"links path {links_path}")
-    print(f"page size limit bytes {page_size_limit_bytes}")
-    print(f"hits per page limit {hits_per_page_limit}")
-    print(f"links per page limit {links_per_page_limit}")
-    print(f"frontier list size {len(frontier)}")
-    print(f"seen set size {len(seen)}")
+    logging.info("exit summary")
+    logging.info(f"hits path {args.hits_path}")
+    logging.info(f"links path {args.links_path}")
+    logging.info(f"page size limit bytes {args.page_size_limit_bytes}")
+    logging.info(f"hits per page limit {args.hits_per_page_limit}")
+    logging.info(f"links per page limit {args.links_per_page_limit}")
+    logging.info(f"frontier list size {len(frontier)}")
+    logging.info(f"seen set size {len(seen)}")
+    logging.info(f"robots map size {len(robots)}")
+    logging.info(f"downloaded count {count}")
 atexit.register(log)
 
-
 # crawl
-with open(hits_path, "w") as hits_file, open(links_path, "w") as links_file:
+with open(args.hits_path, "w") as hits_file, open(args.links_path, "w") as links_file:
     while (True):
         url = frontier.pop(0)
 
@@ -56,38 +59,38 @@ with open(hits_path, "w") as hits_file, open(links_path, "w") as links_file:
             robots[url.host] = Robot(url)
         robot = robots[url.host]
         if robot.disallows(url):
-            print(f"filtered: disallowed {url}")
+            logging.info(f"filtered: disallowed {url}")
             continue
         if robot.delays():
-            print(f"delayed: {url}")
+            logging.info(f"delayed: {robot.last_accessed} {robot.delay} {url}")
             frontier.append(url)
             continue
             
         # HEAD check
-        head = requests.head(url, headers={"user-agent": "tinysearchpython"}, timeout=3)
+        head = requests.head(url, headers={"user-agent": "tinysearchpython"}, timeout=3, allow_redirects=True)
         if head.status_code != requests.codes.ok:
-            print(f"filtered: head status code {head.status_code} {url}")
+            logging.info(f"filtered: head status code {head.status_code} {url}")
             continue
         if 'content-type' not in head.headers:
-            print(f"filtered: no content-type {url}")
+            logging.info(f"filtered: no content-type {url}")
             continue
         if not head.headers['content-type'].startswith("text/html"):
-            print(f"filtered: bad content-type {url}")
+            logging.info(f"filtered: bad content-type {url}")
             continue
         if 'content-length' not in head.headers:
-            print(f"filtered: no content-length {url}")
+            logging.info(f"filtered: no content-length {url}")
             continue
-        if int(head.headers['content-length']) > page_size_limit_bytes:
-            print(f"filtered: bad content-length {head.headers['content-length']} {url}")
+        if int(head.headers['content-length']) > args.page_size_limit_bytes:
+            logging.info(f"filtered: bad content-length {head.headers['content-length']} {url}")
             continue
         
         # GET
-        get = requests.get(url, headers={"user-agent": "tinysearchpython"}, timeout=3)
+        get = requests.get(url, headers={"user-agent": "tinysearchpython"}, timeout=3, allow_redirects=True)
         if get.status_code != requests.codes.ok:
-            print(f"filtered: get status code {get.status_code} {url}")
+            logging.info(f"filtered: get status code {get.status_code} {url}")
             continue
-        
-        print(f"{count} downloaded: {url}")
+        robot.update_last_accessed()
+        logging.info(f"{count} downloaded: {url}")
         count += 1
 
         # parse links
@@ -95,12 +98,12 @@ with open(hits_path, "w") as hits_file, open(links_path, "w") as links_file:
         html.make_links_absolute(str(url))
         links_parsed = 0
         for _, _, link, _ in html.iterlinks():
-            if links_parsed > links_per_page_limit:
+            if links_parsed > args.links_per_page_limit:
                 break
             try:
                 new_url = Url(link)
             except Exception as e:
-                print(f"filtered: malformed url {link}")
+                logging.info(f"filtered: malformed url {link}")
                 continue
             else:
                 print(url, new_url, file=links_file)
@@ -120,7 +123,7 @@ with open(hits_path, "w") as hits_file, open(links_path, "w") as links_file:
         text = text.replace(".", " ")
         links_parsed = 0
         for word in get.text.split():
-            if links_parsed > links_per_page_limit:
+            if links_parsed > args.links_per_page_limit:
                 break
             if not re.fullmatch("[a-zA-Z']+", word):
                 continue
--- tinysearchpython/crawler/robot.py
+++ tinysearchpython/crawler/robot.py
@@ -14,11 +14,10 @@ class Robot:
     def __init__(self, url):
         self.delay = 1
         self.disallowed_paths = set()
-        self.last_accessed = time.time()
 
         robot_url = copy.copy(url)
         robot_url.path = "/robots.txt"
-        get = requests.get(robot_url, headers={"user-agent": "tinysearchpython"}, timeout=3)
+        get = requests.get(robot_url, headers={"user-agent": "tinysearchpython"}, timeout=3, allow_redirects=True)
 
         user_agent = ""
         for line in get.text.splitlines():
@@ -35,15 +34,21 @@ class Robot:
                 if line.split(":")[0].strip().lower() == "disallow" and user_agent == "*":
                     self.disallowed_paths.add(line.split(":")[1].strip())
 
+        # dont want to delay on the request that made us download robots.txt
+        self.last_accessed = time.time() - self.delay
+
     def delays(self):
         return time.time() - self.last_accessed < float(self.delay)
     
     def disallows(self, url):
         return any(url.path.startswith(somepath) for somepath in self.disallowed_paths)
 
+    def update_last_accessed(self):
+        self.last_accessed = time.time()
+
                 
 if __name__ == "__main__":
-    robot = Robot(Url("https://google.com"))
+    robot = Robot(Url("https://cv.wikipedia.org:443/"))
     print(robot.delay)
     print(robot.disallowed_paths)
     print(robot.last_accessed)
